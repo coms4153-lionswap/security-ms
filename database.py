50b49dabebf8f669f1caf9543d0622e194a22520
wenhema <mawenhe@Mas-MacBook-Pro.local>
2025-12-09 15:54:24 -0500

    cahnge port

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = 3306
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
