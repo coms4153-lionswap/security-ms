4f22010ef327514813cece7533140e1abf3397a2
wenhema <mawenhe@Mas-MacBook-Pro.local>
2025-12-09 18:38:30 -0500

    haha

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from models import LoginRequest, LoginResponse
 from auth import login
 from fastapi import HTTPException
 from jose import jwt, JWTError
+from pydantic import BaseModel
 
+class TokenRequest(BaseModel):
+    token: str
 app = FastAPI(title="Security Service",
               description="Handles Google OAuth verification and generates internal JWT",
               version="1.0.0")
@@ -15,14 +18,29 @@ ALGO = "HS256"
 def login_route(request: LoginRequest):
      return login(request.google_access_token)
 
+# @app.post("/security/decode")
+# def decode_token(token: str):
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+#         return {
+#         "user_id": payload.get("user_id"),
+#         "uni" : payload.get("uni"),
+#         "role": payload.get("role")
+#     }
+#     except JWTError:
+#         raise HTTPException(401, "Invalid or expired token")
+
 @app.post("/security/decode")
-def decode_token(token: str):
+def decode_token(req: TokenRequest):
+    # 2. 从模型中取出 token
+    token = req.token 
+    
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
         return {
-        "user_id": payload.get("user_id"),
-        "uni" : payload.get("uni"),
-        "role": payload.get("role")
-    }
+            "user_id": payload.get("user_id"),
+            "uni" : payload.get("uni"),
+            "role": payload.get("role")
+        }
     except JWTError:
         raise HTTPException(401, "Invalid or expired token")
